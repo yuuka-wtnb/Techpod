9da377c80e1303ab2336653de9aacf2b7591c865
Yuka Watanabe <yuuka.wtnb.011@gmail.com>
2020-09-14 15:12:44 +0900

    Initial Commit

---
+++ Techpod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Techpod
+//
+//  Created by Yuuka Watanabe on 2020/09/14.
+//  Copyright © 2020 Yuka Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
